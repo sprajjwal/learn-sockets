dedc9f1e8f8bbd58efd34dee2c6ef9f7bd82b48d
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-27 16:49:44 -0700

    users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -1,32 +1,49 @@
-// index.js
-// $(document).ready(()=>{
-//   const socket = io.connect();
-
-//   $('#create-user-btn').click((e)=>{
-//     e.preventDefault();
-//     let username = $('#username-input').val();
-//     if(username.length > 0){
-//       //Emit to the server the new user
-//       socket.emit('new user', username);
-//       $('.username-form').remove();
-//     }
-//   });
-
-// })
-
 const socket = io.connect();
 const btn = document.getElementById("create-user-btn");
 const username = document.getElementById("username-input");
 const userForm = document.getElementById('username-form');
+const mainContainer = document.querySelector('.main-container');
+const usersOnline = document.querySelector('.users-online');
+const sendChatBtn = document.getElementById('send-chat-btn');
+const messageContainer = document.querySelector('.message-container');
+
+let currentUser;
 
 btn.addEventListener('click', (e)=> {
   if (username.value.length > 0) {
     socket.emit('new user', username.value)
+    currentUser = username.value;
     username.value = ""
+    mainContainer.style.display = 'flex'
+    userForm.style.display = 'none'
+  }
+})
+
+sendChatBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  let message = document.getElementById('chat-input')
+  console.log("message", message.value)
+  if(message.value.length > 0){
+    // Emit the message with the current user to the server
+    socket.emit('new message', {
+      sender : currentUser,
+      message : message.value,
+    });
+    message.value = ""
   }
 })
 
 // socket listeners
 socket.on('new user', (username) => {
   console.log(`✋ ${username} has joined the chat! ✋`);
+  usersOnline.innerHTML += `<div class="user-online">${username}</div>`
+})
+
+socket.on('new message', (data) => {
+  messageContainer.innerHTML += `
+    <div class="message">
+      <p class="message-user">${data.sender}: </p>
+      <p class="message-text">${data.message}</p>
+    </div>
+  `
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -7,4 +7,11 @@ module.exports = (io, socket) => {
     io.emit("new user", username);
   })
 
+  //Listen for new messages
+  socket.on('new message', (data) => {
+    // Send that data back to ALL clients
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+    io.emit('new message', data);
+  })
+
 }
