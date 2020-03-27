51107ab6c6f20c57a859e8b6200cbfaa2b5841f3
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-27 14:15:19 -0700

    sockets connected

---
+++ index.js
@@ -0,0 +1,27 @@
+//app.js
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
+
+//app.js
+//Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars');
+})
+
+server.listen('3000', () => {
+  console.log('Server listening on Port 3000');
+})
