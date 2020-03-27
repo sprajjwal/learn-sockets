4ce6d0783000d39f17ea28a6269df9b71f0330aa
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-27 15:51:25 -0700

    users can now connect

--- public/index.js
+++ public/index.js
@@ -1,4 +1,32 @@
-$(document).ready( () => {
-  //Connect to the socket.io server
-  const socket = io.connect();
+// index.js
+// $(document).ready(()=>{
+//   const socket = io.connect();
+
+//   $('#create-user-btn').click((e)=>{
+//     e.preventDefault();
+//     let username = $('#username-input').val();
+//     if(username.length > 0){
+//       //Emit to the server the new user
+//       socket.emit('new user', username);
+//       $('.username-form').remove();
+//     }
+//   });
+
+// })
+
+const socket = io.connect();
+const btn = document.getElementById("create-user-btn");
+const username = document.getElementById("username-input");
+const userForm = document.getElementById('username-form');
+
+btn.addEventListener('click', (e)=> {
+  if (username.value.length > 0) {
+    socket.emit('new user', username.value)
+    username.value = ""
+  }
+})
+
+// socket listeners
+socket.on('new user', (username) => {
+  console.log(`✋ ${username} has joined the chat! ✋`);
 })
---
+++ sockets/chat.js
@@ -0,0 +1,10 @@
+module.exports = (io, socket) => {
+
+  // Listen for "new user" socket emits
+  socket.on('new user', (username) => {
+    console.log(`✋ ${username} has joined the chat! ✋`);
+    //Send the username to all clients currently connected
+    io.emit("new user", username);
+  })
+
+}
